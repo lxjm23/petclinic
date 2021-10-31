f882d393906fe5840ab0e937cb0e9ed31183df0f
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2021-10-31 02:18:51 -0400

    Lab 3 POGO Models

---
+++ src/main/java/ca/gbc/comp3095/petclinic/model/Pet.java
@@ -0,0 +1,12 @@
+package ca.gbc.comp3095.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    // associates pet to petType and Owner
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthdate;
+
+}
