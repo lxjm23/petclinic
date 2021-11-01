04705bf3f59f7abc7fb47fc46f089681b6c69e28
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2021-11-01 02:28:02 -0400

    Creates pet clinic Services. Closes #5

---
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package ca.gbc.comp3095.petclinic.services;
+
+import ca.gbc.comp3095.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
