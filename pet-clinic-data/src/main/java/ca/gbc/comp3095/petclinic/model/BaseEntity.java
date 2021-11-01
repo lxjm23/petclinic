ee967fc549a1d187e9fe07c75ac540d4283491c3
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2021-11-01 02:14:34 -0400

    Creates BaseEntity. Closes #12

---
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package ca.gbc.comp3095.petclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package ca.gbc.comp3095.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     // associates pet to petType and Owner
     private PetType petType;
