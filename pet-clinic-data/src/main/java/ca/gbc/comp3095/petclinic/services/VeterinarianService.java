cbb18f0c1e830914eec6e10a921887fb7ea530ee
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2021-11-03 08:09:01 -0400

    services added

--- pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/VeterinarianService.java
+++ pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/VeterinarianService.java
@@ -1,4 +1,15 @@
 package ca.gbc.comp3095.petclinic.services;
 
+
+import ca.gbc.comp3095.petclinic.model.Veterinarian;
+
+import java.util.Set;
+
 public interface VeterinarianService {
+
+    Veterinarian findById(Long id);
+    Veterinarian save(Veterinarian veterinarian);
+    Set<Veterinarian> findAll();
+
+
 }
